f407aff9a98a7d2589c41a36b6277f94d1e5084f
ahmethamdibayrak <ahmethamdibayrak@hotmail.com>
2020-11-03 01:04:11 +0300

    template basic

---
+++ config/shop.php
@@ -0,0 +1,13 @@
+<?php
+return [
+	'client' => [
+		'html' => [
+			'common' => [
+				'baseurl' => 'packages/swordbros/shop/themes/frigian' ,
+				'template' => [
+					'baseurl' => public_path( 'packages/swordbros/shop/themes/frigian' ),
+				],
+			],
+		],
+	],
+];
