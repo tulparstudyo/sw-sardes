a038c58a18962b6654db0c1e20fe6125306bf6ac
ahmethamdibayrak <ahmethamdibayrak@hotmail.com>
2021-02-27 14:00:40 +0300

    Xlsx Job

--- admin/jqadm/themes/custom.js
+++ admin/jqadm/themes/custom.js
@@ -20,8 +20,20 @@ $(document).ready(function(){
 	$.get( "/admin/default/jqadm/get/swordbros/sardes/admin-bar", function( data ) {
 		$('.app-menu').prepend(data);
 	});
+    let url = window.location.href;
+    if(url.indexOf('jqadm/search/log')>0){
+        $.get( "/admin/default/jqadm/get/swordbros/sardes/main-navbar-log", function( data ) {
+            $('.main-navbar.log').append(data);
+        });
+    } else if(url.indexOf('jqadm/search/product')>0){
+        $.get( "/admin/default/jqadm/get/swordbros/sardes/main-navbar-product", function( data ) {
+            $('.main-navbar').append(data);
+        });
+    }
 	$('body').on('change', '.sardes-img .image-file', function(){
 		loadLocaleImage(this);
 	});
-
+    $('body').on('change', '#excel-file', function(){
+        $('#export-product').submit();
+    });
 });
